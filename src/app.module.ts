a3017dc6fa032a786aff7a515b8982efe889c0ac
aabi01 <abdullah.amalik07@gmail.com>
2025-02-07 13:07:44 +0100

    add db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig } from './config/database.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(databaseConfig),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
