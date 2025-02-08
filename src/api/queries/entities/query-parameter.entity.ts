56a937daf96b1dd214a417a1d2a55d4610e36a2d
aabi01 <abdullah.amalik07@gmail.com>
2025-02-08 12:20:02 +0100

    add query migration

--- src/api/queries/entities/query-parameter.entity.ts
+++ src/api/queries/entities/query-parameter.entity.ts
@@ -1,22 +1,25 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { Query } from './query.entity';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class QueryParameter {
-  @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ApiProperty()
   @Column()
   name: string;
 
-  @ApiProperty()
   @Column()
   value: string;
 
-  @ManyToOne(() => Query, query => query.parameters, { onDelete: 'CASCADE' })
+  @ManyToOne(() => Query, (query) => query.parameters, { onDelete: 'CASCADE' })
   query: Query;
 
   @CreateDateColumn()
