2d04894d1d65801cdb2d58812016dcf89083f0aa
aabi01 <abdullah.amalik07@gmail.com>
2025-02-08 09:21:43 +0100

    add index.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { databaseConfig } from './config/database.config';
-import { UsersModule } from './api/users/users.module';
-import { QueriesModule } from './api/queries/queries.module';
-import { BooksModule } from './api/books/books.module';
+import { UsersModule, QueriesModule, BooksModule } from './api';
 
 @Module({
   imports: [
